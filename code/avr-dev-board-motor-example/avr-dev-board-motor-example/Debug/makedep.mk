980ea5d78d7c4bdac7f6a84e2ed25159d66e81ea
markotikvic <markotikvic@gmail.com>
2015-09-10 23:17:14 +0200

    MOTOR

---
+++ code/avr-dev-board-motor-example/avr-dev-board-motor-example/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+avr-dev-board-motor-example.c
+
+counter.c
+
+gpio.c
+
+motor.c
+
+timer.c
+
+uart.c
+
+usartlib.c
+
+util.c
+
