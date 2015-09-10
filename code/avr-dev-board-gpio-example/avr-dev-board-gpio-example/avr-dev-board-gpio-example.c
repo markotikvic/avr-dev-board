d7d1cd248a2cd6678b5a08adaf2dc51d0aa81376
markotikvic <markotikvic@gmail.com>
2015-09-10 20:53:36 +0200

    added code directory

---
+++ code/avr-dev-board-gpio-example/avr-dev-board-gpio-example/avr-dev-board-gpio-example.c
@@ -0,0 +1,17 @@
+/*
+ * avr_dev_board_gpio_example.c
+ *
+ * Created: 10-Sep-15 20:47:33
+ *  Author: marko
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
