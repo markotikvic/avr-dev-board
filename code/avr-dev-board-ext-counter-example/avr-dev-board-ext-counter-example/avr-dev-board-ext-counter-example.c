7db4ea274d4ce2b80f1875cc436610b0ef370f17
markotikvic <markotikvic@gmail.com>
2015-09-12 23:03:51 +0200

    ext counter example started

---
+++ code/avr-dev-board-ext-counter-example/avr-dev-board-ext-counter-example/avr-dev-board-ext-counter-example.c
@@ -0,0 +1,16 @@
+/*
+ * avr_dev_board_ext_counter_example.c
+ *
+ * Created: 12-Sep-15 22:57:54
+ *  Author: marko
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1){
+        //TODO:: Please write your application code 
+    }
+}
