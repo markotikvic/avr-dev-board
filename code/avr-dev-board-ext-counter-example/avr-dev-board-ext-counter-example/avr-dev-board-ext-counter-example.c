a631d0ffa93ff93e14131538a0008522f29a2e15
markotikvic <markotikvic@gmail.com>
2015-09-13 14:34:11 +0200

    ext counter completed

--- code/avr-dev-board-ext-counter-example/avr-dev-board-ext-counter-example/avr-dev-board-ext-counter-example.c
+++ code/avr-dev-board-ext-counter-example/avr-dev-board-ext-counter-example/avr-dev-board-ext-counter-example.c
@@ -3,14 +3,35 @@
  *
  * Created: 12-Sep-15 22:57:54
  *  Author: marko
+ *  
+ *  Primer upotrebe counter.h biblioteke:
+ *		Na svaku sekundu kontroler putem UART komunikacije salje trenutno stanje
+ *		brojaca. Pre slanja, podatak moramo pretvoriti iz tipa long u tip char*
+ *		upotrebom spintf funkcije koja se nalazi u stdio.h biblioteci.
  */ 
 
-
 #include <avr/io.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "counter.h"
+#include "uart.h"
+#include "timer.h"
 
 int main(void)
 {
+	init_uart(UART_9600);
+	init_timer_1(PRESCALER8, 1000);
+	enable_counter();
+	char s[10] = {'\0'};
+	
     while(1){
-        //TODO:: Please write your application code 
+        send_string("Hello World from AVR!\n\r");
+		send_string("Counts: ");
+		sprintf(s, "%li", get_count());
+		send_string(s);
+		send_string("\n\r");
+		s[0] = '\0';
+		pause_loop(1000);
     }
 }
